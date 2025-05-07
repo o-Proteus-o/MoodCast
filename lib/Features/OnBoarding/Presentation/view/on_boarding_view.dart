ba6724614ca1bf7ce4ace50ec808c0d41f9ceb51
Proteus <107575501+o-Proteus-o@users.noreply.github.com>
2025-05-07 09:51:23 +0100

    add image path to the onboarding pageview

--- lib/Features/OnBoarding/Presentation/view/on_boarding_view.dart
+++ lib/Features/OnBoarding/Presentation/view/on_boarding_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:moodcast/Features/Auth/Presentation/View/login/login_view.dart';
 
 class OnBoardingView extends StatefulWidget {
@@ -11,19 +12,23 @@ class OnBoardingView extends StatefulWidget {
 
 class _OnBoardingViewState extends State<OnBoardingView> {
   final PageController _pageController = PageController();
+
   final List<Map<String, String>> onboardingData = [
     {
       "title": "🌤️ Welcome to MoodCast",
+      // "image": "assets/weather/one.svg",
       "description":
           "Discover how the weather affects your mood.\nMoodCast helps you tune in, reflect, and \nstay balanced—rain or shine.",
     },
     {
       "title": "🌦️ Mood Meets Weather",
+      // "image": "assets/weather/two.svg",
       "description":
           "We combine local weather data with \nemotional insightsto help you track patterns, \nreflect, and understand yourself better.",
     },
     {
       "title": "🌈 Start Your Mood Journey",
+      // "image": "assets/weather/four.svg",
       "description":
           "Ready to see how sunshine lifts your \nspirits—or how rain soothes your soul? \nLet’s personalize your experience.",
     },
@@ -53,6 +58,7 @@ class _OnBoardingViewState extends State<OnBoardingView> {
                     style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                     textAlign: TextAlign.center,
                   ),
+                  // Image.asset(onboardingData[index]["image"]!),
                   // SizedBox(height: 16),
                   Text(
                     onboardingData[index]['description']!,
@@ -71,13 +77,18 @@ class _OnBoardingViewState extends State<OnBoardingView> {
           Positioned(
             top: MediaQuery.of(context).size.height * 0.904,
             bottom: 40,
-            left: MediaQuery.of(context).size.width * 0.25,
-            right: MediaQuery.of(context).size.width * 0.25,
+            left:
+                _currentPage == onboardingData.length - 1
+                    ? MediaQuery.of(context).size.width * 0.25
+                    : MediaQuery.of(context).size.width * 0.80,
+            right:
+                _currentPage == onboardingData.length - 1
+                    ? MediaQuery.of(context).size.width * 0.25
+                    : MediaQuery.of(context).size.width * 0.05,
             child: GestureDetector(
               onTap: () {
                 if (_currentPage == onboardingData.length - 1) {
-                  // Navigate to the next screen
-                  Get.to(LoginView(), transition: Transition.cupertino);
+                  Get.to(LoginView(), transition: Transition.fade);
                 } else {
                   _pageController.nextPage(
                     duration: Duration(milliseconds: 300),
@@ -90,26 +101,31 @@ class _OnBoardingViewState extends State<OnBoardingView> {
               },
               child: Container(
                 padding: EdgeInsets.only(
-                  left: 16,
-                  right: 16,
+                  left: _currentPage == onboardingData.length - 1 ? 16 : 8,
+                  right: _currentPage == onboardingData.length - 1 ? 16 : 8,
                   top: 8,
                   bottom: 8,
                 ),
                 decoration: BoxDecoration(
                   color: Colors.blueGrey,
-                  borderRadius: BorderRadius.circular(8),
+                  borderRadius: BorderRadius.circular(15),
                 ),
-                child: Text(
-                  _currentPage == onboardingData.length - 1
-                      ? "Get Started"
-                      : "Next",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 22,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+                child:
+                    _currentPage == onboardingData.length - 1
+                        ? Text(
+                          "Get Started",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 22,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                        : Icon(
+                          LucideIcons.arrowRight,
+                          size: 28,
+                          color: Colors.white,
+                        ),
               ),
             ),
           ),
