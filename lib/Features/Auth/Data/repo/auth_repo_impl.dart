9b2dde7117ad5fd79d66015ab58b8e0962e33480
Proteus <107575501+o-Proteus-o@users.noreply.github.com>
2025-05-14 16:36:32 +0100

    pass the data

--- lib/Features/Auth/Data/repo/auth_repo_impl.dart
+++ lib/Features/Auth/Data/repo/auth_repo_impl.dart
@@ -1,15 +1,32 @@
 import 'package:dartz/dartz.dart';
 import 'package:moodcast/Core/Errors/failures.dart';
+import 'package:moodcast/Core/Functions/my_exeptions.dart';
+import 'package:moodcast/Core/Services/firebase_auth_services.dart';
+import 'package:moodcast/Features/Auth/Data/Models/user_model.dart';
 import 'package:moodcast/Features/Auth/Domain/entities/user_entity.dart';
 import 'package:moodcast/Features/Auth/Domain/repo/auth_repo.dart';
 
 class AuthRepoImpl extends AuthRepo {
+  final FirebaseAuthServices firebaseAuthServices;
+
+  AuthRepoImpl({required this.firebaseAuthServices});
   @override
-  Future<Either<Failure, UserEntitny>> signInWithEmailAndPassword({
-    required String email,
-    required String password,
-  }) {
-    throw UnimplementedError();
+  Future<Either<Failure, UserEntitny>> signInWithEmailAndPassword(
+    String email,
+    String password,
+  ) async {
+    try {
+      var user = await firebaseAuthServices.createWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      return right(UserModel.fromFirebaseUser(user));
+    } on MyExeption catch (e) {
+      return left(ServerFailure(e.toString()));
+    } catch (e) {
+      return left(ServerFailure("There was an error signing in"));
+    }
   }
 
   @override
@@ -18,11 +35,11 @@ class AuthRepoImpl extends AuthRepo {
   }
 
   @override
-  Future<Either<Failure, UserEntitny>> signUpWithEmailAndPassword({
-    required String name,
-    required String email,
-    required String password,
-  }) {
+  Future<Either<Failure, UserEntitny>> signUpWithEmailAndPassword(
+    String name,
+    String email,
+    String password,
+  ) {
     throw UnimplementedError();
   }
 }
--- lib/Features/Auth/Domain/repo/auth_repo.dart
+++ lib/Features/Auth/Domain/repo/auth_repo.dart
@@ -3,16 +3,16 @@ import 'package:moodcast/Core/Errors/failures.dart';
 import 'package:moodcast/Features/Auth/Domain/entities/user_entity.dart';
 
 abstract class AuthRepo {
-  Future<Either<Failure, UserEntitny>> signInWithEmailAndPassword({
-    required String email,
-    required String password,
-  });
+  Future<Either<Failure, UserEntitny>> signInWithEmailAndPassword(
+    String email,
+    String password,
+  );
 
-  Future<Either<Failure, UserEntitny>> signUpWithEmailAndPassword({
-    required String name,
-    required String email,
-    required String password,
-  });
+  Future<Either<Failure, UserEntitny>> signUpWithEmailAndPassword(
+    String name,
+    String email,
+    String password,
+  );
 
   Future<void> signOut();
 }
