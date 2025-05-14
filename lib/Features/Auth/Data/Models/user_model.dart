946b17e892c5a52b472e28e9d9f74be54d5b4fae
Proteus <107575501+o-Proteus-o@users.noreply.github.com>
2025-05-14 16:36:02 +0100

    parse the data with user model

---
+++ lib/Features/Auth/Data/Models/user_model.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:moodcast/Features/Auth/Domain/entities/user_entity.dart';
+
+class UserModel extends UserEntitny {
+  UserModel({required super.id, required super.name, required super.email});
+
+  factory UserModel.fromFirebaseUser(User user) {
+    return UserModel(
+      id: user.uid,
+      name: user.displayName ?? '',
+      email: user.email ?? '',
+    );
+  }
+}
