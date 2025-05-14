926a17ee9dc414c0b43aeeebb210e010330a3ef3
Proteus <107575501+o-Proteus-o@users.noreply.github.com>
2025-05-14 19:51:46 +0100

    navigate to the home view with GetX

--- lib/Features/Auth/Presentation/View/login/widgets/login_bloc_consumer.dart
+++ lib/Features/Auth/Presentation/View/login/widgets/login_bloc_consumer.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:moodcast/Features/Auth/Presentation/Manager/login_cubit/login_cubit.dart';
 import 'package:moodcast/Features/Auth/Presentation/View/login/widgets/login_body.dart';
+// import 'package:moodcast/Features/Home/Presentation/Views/home_view/home_view.dart';
 
 class LoginBlocConsumer extends StatelessWidget {
   const LoginBlocConsumer({super.key});
@@ -14,6 +15,7 @@ class LoginBlocConsumer extends StatelessWidget {
       listener: (context, state) {
         if (state is LoginSuccess) {
           Get.snackbar("Hello", "You're In");
+          // Get.to(HomeView());
         }
         if (state is LoginFailure) {
           Get.snackbar("Error", state.message);
