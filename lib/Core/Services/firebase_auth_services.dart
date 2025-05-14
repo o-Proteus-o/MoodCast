55d7c106226df49aec1f0826ea37cea8a5a20b34
Proteus <107575501+o-Proteus-o@users.noreply.github.com>
2025-05-14 16:14:24 +0100

    done with the firebase service

--- lib/Core/Services/firebase_auth_services.dart
+++ lib/Core/Services/firebase_auth_services.dart
@@ -1,1 +1,28 @@
+import 'dart:developer';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:moodcast/Core/Functions/my_exeptions.dart';
+
+class FirebaseAuthServices {
+  // FirebaseAuth instance
+  Future<User> createWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw MyExeption('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        throw MyExeption('The account already exists for that email.');
+      }
+      throw MyExeption("There was an error creating the account");
+    } catch (e) {
+      log('Error: $e');
+      throw MyExeption("There was an error creating the account");
+    }
+  }
+}
