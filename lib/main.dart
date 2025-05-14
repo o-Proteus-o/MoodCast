2a6b44676865c397ecf54b2eb4d6aab01b139772
Proteus <107575501+o-Proteus-o@users.noreply.github.com>
2025-05-14 15:58:21 +0100

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:moodcast/Core/Utils/Routes/on_generated_routes.dart';
 import 'package:moodcast/Features/OnBoarding/Presentation/view/on_boarding_view.dart';
+import 'package:moodcast/firebase_options.dart';
 // import 'package:moodcast/Features/Splash/Presentation/views/splash_veiw.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
